31b95e35236dd056930d41af6f7e43da2de387e6
Murage-Ivy <ivymurage2000@gmail.com>
2023-03-30 15:52:42 +0300

    Implement log in and log out methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: user_params[:email])
+    if user&.authenicate(user_params[:password])
+      session[:user_id] = user_id
+      render json: user, status: :created
+    else
+      render json: { errors: ["Invalid email or password"] }, status: :unauthorized
+    end
+  end
+
+  def destroy
+    session.delete :user_id
+    head :no_content
+  end
 end
