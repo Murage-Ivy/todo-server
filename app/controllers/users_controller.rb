c802fb0425e4ae8cc20d413f49e20f37836d2a15
Murage-Ivy <ivymurage2000@gmail.com>
2023-03-30 15:39:58 +0300

    Sign up user works successfully

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   skip_before_action :authorized, only: [:create]
+  #   wrap_parameters format: []
 
   def create
     @user = User.create!(user_params)
-    session[:user_id] = @user.index
+    session[:user_id] = @user.id
     render json: @user, status: :created
   end
 
@@ -18,6 +19,6 @@ class UsersController < ApplicationController
   end
 
   def render_unprocessable_entity_response(invalid)
-    render json: { errors: invalid.errors.full_message }, status: :unprocessable_entity
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 end
