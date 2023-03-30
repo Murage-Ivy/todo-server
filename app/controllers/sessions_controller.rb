31f18917251180864ab11da5e7151e1cacffdbd0
Murage-Ivy <ivymurage2000@gmail.com>
2023-03-30 15:55:57 +0300

    Test login for user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_by(email: user_params[:email])
-    if user&.authenicate(user_params[:password])
-      session[:user_id] = user_id
+    if user&.authenticate(user_params[:password])
+      session[:user_id] = user.id
       render json: user, status: :created
     else
       render json: { errors: ["Invalid email or password"] }, status: :unauthorized
@@ -12,5 +12,11 @@ class SessionsController < ApplicationController
   def destroy
     session.delete :user_id
     head :no_content
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password)
   end
 end
