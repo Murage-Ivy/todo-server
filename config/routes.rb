77b2a1539e903206d3a9cc41c3517c33e97bc909
Murage-Ivy <ivymurage2000@gmail.com>
2023-04-03 13:19:51 +0300

    Add todos on user fetch

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :todos, only: [:index, :show, :create, :destroy, :update]
-  resources :users, only: [:create]
+  resources :users, only: [:create] do
+    resources :todos, only: [:index]
+  end
 
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
